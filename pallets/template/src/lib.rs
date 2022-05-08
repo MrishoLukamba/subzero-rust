f92e74f292acf0398342285d1401709dccd139ad
MrishoLukamba <abdulrazzaqlukamba@gmail.com>
2022-05-08 06:21:54 -0700

    quiz

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,23 +1,18 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://docs.substrate.io/v3/runtime/frame>
-pub use pallet::*;
 
-#[cfg(test)]
-mod mock;
 
-#[cfg(test)]
-mod tests;
-
-#[cfg(feature = "runtime-benchmarks")]
-mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::pallet_prelude::*;
+	use frame_support::{
+		dispatch::DispatchResult,
+		pallet_prelude::*,
+		storage::bounded_vec::BoundedVec,
+
+	};
 	use frame_system::pallet_prelude::*;
+
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
@@ -27,76 +22,68 @@ pub mod pallet {
 	}
 
 	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
+	#[pallet::generate_store(pub (super) trait Store)]
 	pub struct Pallet<T>(_);
 
-	// The pallet's runtime storage items.
-	// https://docs.substrate.io/v3/runtime/storage
-	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
 
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/v3/runtime/events-and-errors
+	#[pallet::storage]
+	#[pallet::getter(fn get_value)]
+	pub type Number<T> = StorageValue<_, u32,ValueQuery>;
+
+
 	#[pallet::event]
-	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	#[pallet::generate_deposit(pub (super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored(u32, T::AccountId),
+		IncrementedNumber(u32,T::AccountId),
+		DecrementedNumber(u32,T::AccountId),
+		NumberStored(u32, T::AccountId),
 	}
 
 	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
+		StorageUnderflow,
 		StorageOverflow,
 	}
 
-	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/v3/runtime/origins
-			let who = ensure_signed(origin)?;
+		#[pallet::weight(1000)]
+		pub fn get_number(origin:OriginFor<T>) -> DispatchResult{
+			let owner = ensure_signed(origin)?;
 
-			// Update storage.
-			<Something<T>>::put(something);
+			let number = Self::get_value();
 
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something, who));
-			// Return a successful DispatchResultWithPostInfo
+			Self::deposit_event(Event::NumberStored(number, owner));
 			Ok(())
 		}
 
-		/// An example dispatchable that may throw a custom error.
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+		#[pallet::weight(1000)]
+		pub fn increment(origin:OriginFor<T>, number:u32) -> DispatchResult{
+			let owner = ensure_signed(origin)?;
 
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
+			let count = Self::get_value().checked_add(number).ok_or(<Error<T>>::StorageOverflow)?;
+
+			<Number<T>>::put(count);
+			Self::deposit_event(Event::IncrementedNumber(count,owner));
+			Ok(())
+
+		}
+
+		#[pallet::weight(1000)]
+		pub fn decrement(origin:OriginFor<T>, number:u32) -> DispatchResult{
+			let owner = ensure_signed(origin)?;
+
+			let count = Self::get_value().checked_sub(number).ok_or(<Error<T>>::StorageUnderflow)?;
+
+			<Number<T>>::put(count);
+			Self::deposit_event(Event::DecrementedNumber(count,owner));
+
+			Ok(())
+
 		}
 	}
-}
+  }
+
+
